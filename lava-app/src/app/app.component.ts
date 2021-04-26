a5100c10b63e962f2e47fe507122faf867e06ed0
Lars Vad <larsvad@yahoo.dk>
2021-04-26 14:21:43 +0200

    Small changes

--- lava-app/src/app/app.component.ts
+++ lava-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lava-app';
+  description = 'This is a description'
+  today = new Date()
+  magicNumber = 42 
 }
